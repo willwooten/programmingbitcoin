e7232acf49a4411f09427f424f80290b84b4be4c
willwooten <wwootencfc@gmail.com>
2021-05-17 16:18:42 -0500

    ch6 ch7

--- code-ch06/op.py
+++ code-ch06/op.py
@@ -666,14 +666,26 @@ def op_hash256(stack):
 
 def op_checksig(stack, z):
     # check that there are at least 2 elements on the stack
+    if len(stack) < 2:
+        return False
     # the top element of the stack is the SEC pubkey
+    sec_pubkey = stack.pop()
     # the next element of the stack is the DER signature
     # take off the last byte of the signature as that's the hash_type
-    # parse the serialized pubkey and signature into objects
+    der_signature = stack.pop()[:-1]
     # verify the signature using S256Point.verify()
+    # parse the serialized pubkey and signature into objects
+    try:
+        point = S256Point.parse(sec_pubkey)
+        sig = Signature.parse(der_signature)
+    except (ValueError, SyntaxError) as e:
+        return False
     # push an encoded 1 or 0 depending on whether the signature verified
-    raise NotImplementedError
-
+    if point.verify(z, sig):
+        stack.append(encode_num(1))
+    else:
+        stack.append(encode_num(0))
+    return True
 
 def op_checksigverify(stack, z):
     return op_checksig(stack, z) and op_verify(stack)
